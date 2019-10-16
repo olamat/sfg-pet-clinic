b240b87b9de00e6a4b4ca540ee1f88aed6d9f366
olamat <ola.mattsson@gmail.com>
2019-10-16 13:14:19 +0200

    Renamed map implementations to be consistent

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetMapService.java
@@ -12,10 +12,10 @@ import java.util.Set;
  * Created by jt on 7/21/18.
  */
 @Service
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+public class VetMapService extends AbstractMapService<Vet, Long> implements VetService {
     private final SpecialityService specialityService;
     
-    public VetServiceMap(SpecialityService specialityService) {
+    public VetMapService(SpecialityService specialityService) {
         this.specialityService = specialityService;
     }
     
